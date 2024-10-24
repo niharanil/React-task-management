86268679faa269b58ed92e7b8cc65276fd464119
niharanil <90025243+niharanilh@users.noreply.github.com>
2024-10-24 10:17:37 +0530

    refactor

--- src/context/LoginContext.js
+++ src/context/LoginContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState} from 'react';
 import { getUserFromLocalStorage, saveUserToLocalStorage, clearUserFromLocalStorage } from '../utils/localStorageUtils';
 
 export const LoginContext = createContext();
